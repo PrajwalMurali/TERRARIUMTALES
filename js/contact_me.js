b54b83b2c5a5e3f890b7f9adaa9bda6e1a525e62
Prajwal Murali <prajwal@terrariumtales.com>
2026-02-20 09:35:28 +0530

    Integrate Formspree contact form with improved styling and error handling

--- js/contact_me.js
+++ js/contact_me.js
@@ -17,33 +17,37 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "https://formspree.io/f/mdallzwp",
                 type: "POST",
                 data: {
                     name: name,
                     email: email,
                     message: message
                 },
+                dataType: "json",
                 cache: false,
-                success: function() {
+                success: function(response) {
                     // Success message
                     $('#success').html("<div class='alert alert-success'>");
                     $('#success > .alert-success').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
                     $('#success > .alert-success')
-                        .append("<strong>Your message has been sent. </strong>");
+                        .append("<strong>Thank you " + firstName + "! </strong>");
+                    $('#success > .alert-success')
+                        .append("Your message has been sent successfully. We'll get back to you soon!");
                     $('#success > .alert-success')
                         .append('</div>');
 
                     //clear all fields
                     $('#contactForm').trigger("reset");
                 },
-                error: function() {
+                error: function(error) {
                     // Fail message
                     $('#success').html("<div class='alert alert-danger'>");
                     $('#success > .alert-danger').html("<button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;")
                         .append("</button>");
-                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", it seems that my mail server is not responding. Please try again later!");
+                    $('#success > .alert-danger').append("<strong>Sorry " + firstName + ", </strong>");
+                    $('#success > .alert-danger').append("There was an error sending your message. Please try again later or contact us directly at muralimanjum4@gmail.com");
                     $('#success > .alert-danger').append('</div>');
                     //clear all fields
                     $('#contactForm').trigger("reset");
